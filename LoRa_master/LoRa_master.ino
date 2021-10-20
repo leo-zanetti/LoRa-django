b564d66b055d26827c7008a2defca7245e01f172
RadioLeo <75381247+RadioLeo@users.noreply.github.com>
2021-10-20 11:48:24 -0300

    Master - Slaves funcional
    Master - 2 slaves funcional, sem confusão de pacotes mais ainda sem timout caso entre em loop infinito

--- LoRa_master/LoRa_master.ino
+++ LoRa_master/LoRa_master.ino
@@ -1,10 +1,10 @@
 #include "heltec.h"
 #define BAND 915E6
-#define INTERVAL 5000
+
+const unsigned long INTERVAL = 5000;
+unsigned long lastSendTime = 0;
 
 int i = 1;
-int j = 2;
-long lastSendTime = 0;
 
 void setup() {
   Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
@@ -12,46 +12,31 @@ void setup() {
 }
 
 void loop() {
-  delay(INTERVAL);
-  // enviando a requisição do pacote ao slave
-  Serial.print("Enviando requisição ao slave...");
-  Serial.println(i);
-  LoRa.beginPacket();
-  LoRa.setTxPower(14, RF_PACONFIG_PASELECT_PABOOST);
-  LoRa.print(i);
-  LoRa.endPacket();
-  
+  unsigned long currentTime = millis();
+  if (currentTime - lastSendTime > INTERVAL) {
+    // enviando a requisição do pacote ao slave
+    Serial.print("Enviando requisição ao slave...");
+    Serial.println(i);
+    LoRa.beginPacket();
+    LoRa.setTxPower(14, RF_PACONFIG_PASELECT_PABOOST);
+    LoRa.print(i);
+    LoRa.endPacket();
+    lastSendTime = currentTime;
+  }
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
     Serial.print("Recebido o pacote '");
     while (LoRa.available()) {
       String received = LoRa.readString();
       Serial.print(received);
+      // print RSSI of packet
+      Serial.print("' com RSSI igual a ");
+      Serial.println(LoRa.packetRssi());
+      if (received.charAt(0) == '1') {
+        i = 2;
+      } else if (received.charAt(0) == '2') {
+        i = 1;
+      }
     }
-    // print RSSI of packet
-    Serial.print("' com RSSI igual a ");
-    Serial.println(LoRa.packetRssi());
-  }
-  
-  delay(INTERVAL);
-  
-  // enviando a requisição do pacote ao slave
-  Serial.print("Enviando requisição ao slave...");
-  Serial.println(j);
-  LoRa.beginPacket();
-  LoRa.setTxPower(14, RF_PACONFIG_PASELECT_PABOOST);
-  LoRa.print(j);
-  LoRa.endPacket();
-
-  packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    Serial.print("Recebido o pacote '");
-    while (LoRa.available()) {
-      String received = LoRa.readString();
-      Serial.print(received);
-    }
-    // print RSSI of packet
-    Serial.print("' com RSSI igual a ");
-    Serial.println(LoRa.packetRssi());
   }
 }

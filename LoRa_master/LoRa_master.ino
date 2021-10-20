ab94ae92720ecb4c64a1c7a762785ae3566b50f6
RadioLeo <75381247+RadioLeo@users.noreply.github.com>
2021-10-19 22:25:51 -0300

    Recebendo dos dados
    Master recebendo minimamente dados, ainda que de forma confusa, mesclando informações do nó 1 e do nó 2

--- LoRa_master/LoRa_master.ino
+++ LoRa_master/LoRa_master.ino
@@ -1,85 +1,57 @@
 #include "heltec.h"
-
 #define BAND 915E6
+#define INTERVAL 5000
 
+int i = 1;
+int j = 2;
 long lastSendTime = 0;
 
-String packet = "";
-String id;
-String temperature;
-String humidity;
-
-const int count = 0;
-boolean check1;
-boolean check2;
-const String GETDATA = "get";
-const String SETDATA = "set";
-
 void setup() {
-  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.Heltec.Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
-  Heltec.display->init();
-  Heltec.display->flipScreenVertically();
-  Heltec.display->setFont(ArialMT_Plain_10);
-  Heltec.display->clear();
-  Heltec.display->drawString(0, 0, "Heltec.LoRa inicializado com sucesso!");
-  Heltec.display->drawString(0, 20, "Eu sou o master!");
-  Heltec.display->display();
-
-  LoRa.crc();
+  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
   LoRa.receive();
-
-  send();
 }
 
 void loop() {
-  if (check1 == true && check2 == true) {
-    send();
-  } else {
-    receive();
-  }
-}
-
-void send() {
-  //  Serial.println("Enviando request");
-  //Inicializa o pacote
+  delay(INTERVAL);
+  // enviando a requisição do pacote ao slave
+  Serial.print("Enviando requisição ao slave...");
+  Serial.println(i);
   LoRa.beginPacket();
-  //Envia o que está contido em "GETDATA"
-  LoRa.print(GETDATA);
-  //Finaliza e envia o pacote
+  LoRa.setTxPower(14, RF_PACONFIG_PASELECT_PABOOST);
+  LoRa.print(i);
   LoRa.endPacket();
-  check1 = false;
-  check2 = false;
-}
-
-void receive() {
-  //Tentamos ler o pacote
+  
   int packetSize = LoRa.parsePacket();
-
-  //Verificamos se o pacote tem o tamanho mínimo de caracteres que esperamos
-  if (packetSize > SETDATA.length()) {
-    String received = "";
-    //Armazena os dados do pacote em uma string
-    for (int i = 0; i < SETDATA.length(); i++) {
-      received += (char) LoRa.read();
+  if (packetSize) {
+    Serial.print("Recebido o pacote '");
+    while (LoRa.available()) {
+      String received = LoRa.readString();
+      Serial.print(received);
     }
-    //Se o cabeçalho é o que esperamos
-    if (received.equals(SETDATA)) {
-      //Fazemos a leitura dos dados
-      packet = LoRa.readString();
-      //Mostramos os dados no display
-      showData();
-      if (packet.charAt(0) == '1') {
-        Serial.println(packet);
-        check1 = true;
-      } else if (packet.charAt(0) == '2') {
-        Serial.println(packet);
-        check2 = true;
-      }
-    }
+    // print RSSI of packet
+    Serial.print("' com RSSI igual a ");
+    Serial.println(LoRa.packetRssi());
   }
-}
-void showData() {
-  Heltec.display->clear();
-  Heltec.display->drawString(0, 20, packet);
-  Heltec.display->display();
+  
+  delay(INTERVAL);
+  
+  // enviando a requisição do pacote ao slave
+  Serial.print("Enviando requisição ao slave...");
+  Serial.println(j);
+  LoRa.beginPacket();
+  LoRa.setTxPower(14, RF_PACONFIG_PASELECT_PABOOST);
+  LoRa.print(j);
+  LoRa.endPacket();
+
+  packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    Serial.print("Recebido o pacote '");
+    while (LoRa.available()) {
+      String received = LoRa.readString();
+      Serial.print(received);
+    }
+    // print RSSI of packet
+    Serial.print("' com RSSI igual a ");
+    Serial.println(LoRa.packetRssi());
+  }
 }
--- LoRa_slave_12/LoRa_slave_12.ino
+++ LoRa_slave_12/LoRa_slave_12.ino
@@ -3,59 +3,31 @@
 #define BAND 915E6
 
 int sensorPin = 13;
-
-const String GETDATA = "get";
-const String SETDATA = "set";
-
+String received;
 String packet = "";
+String request = "2";
 int id = 2;
 float moisture;
 
 void setup() {
   Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.Heltec.Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
-  Heltec.display->init();
-  Heltec.display->flipScreenVertically();
-  Heltec.display->setFont(ArialMT_Plain_10);
-  Heltec.display->clear();
-
-  Heltec.display->drawString(0, 0, "Heltec.LoRa inicializado!");
-  Heltec.display->drawString(0, 20, "Eu sou o slave 12!");
-  Heltec.display->display();
-  delay(1000);
-
-  Serial.println("LoRa iniciado para o slave 12");
-
-  LoRa.crc();
-  LoRa.receive();
 }
 
 void loop() {
-  //Tenta ler o pacote
   int packetSize = LoRa.parsePacket();
-
-  //Verifica se o pacote possui a quantidade de caracteres que esperamos
-  if (packetSize == GETDATA.length())
-  {
-    String received = "";
-
-    //Armazena os dados do pacote em uma string
-    while (LoRa.available())
-    {
-      received += (char) LoRa.read();
-    }
-    Serial.println(received);
-    if (received.equals(GETDATA))
-    {
-      //Faz a leitura dos dados
-      readData();
-      Serial.println("Criando pacote para envio");
-      //Cria o pacote para envio
-      LoRa.beginPacket();
-      LoRa.print(SETDATA);
-      LoRa.print(packet);
-      //Finaliza e envia o pacote
-      LoRa.endPacket();
-      showSentData();
+  if (packetSize) {
+    while (LoRa.available()) {
+      received = (char)LoRa.read();
+      if (received.equals(request)) {
+        Serial.print("Pacote recebido: ");
+        Serial.println(received);
+        readData();
+        Serial.println("Enviando pacote...");
+        LoRa.beginPacket();
+        LoRa.setTxPower(14, RF_PACONFIG_PASELECT_PABOOST);
+        LoRa.print(packet);
+        LoRa.endPacket();
+      }
     }
   }
 }
@@ -64,16 +36,6 @@ void readData()
 {
   moisture = (100 - ((analogRead(sensorPin) / 1023.00) * 100));
   packet = String(id) + "/" + String(moisture);
-}
-
-void showSentData()
-{
-  //Mostra no display
-  Heltec.display->clear();
-  Heltec.display->drawString(0, 0, "Nó:" + String(id));
-  Heltec.display->drawString(0, 10, "Enviou:");
-  Heltec.display->drawString(0, 20, String(moisture));
-  Heltec.display->display();
-  Serial.print("Enviou:");
+  Serial.print("Medidas empacotadas: ");
   Serial.println(packet);
 }
